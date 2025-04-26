467387d5d922146ae06739dc0c7b40e2203c7d9e
SAGAR PURKUTI <sagar101purkuti@gmail.com>
2025-04-26 17:39:13 +0545

    review and rating implemented successfully

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from category.models import Category
 from django.urls import reverse
 from accounts.models import Account
+from django.db.models import Avg, Count
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
@@ -22,6 +23,20 @@ class Product(models.Model):
 
     def get_url(self):
         return reverse('product_detail', args=[self.category.slug, self.slug])
+    
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+    
+    def countReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count = Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
 
 
 class VariationManager(models.Manager):
@@ -50,7 +65,6 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
-
 
 
 class ReviewRating(models.Model):
