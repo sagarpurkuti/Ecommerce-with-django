cf0b2c4d6663f6156343b3e7ddaf8d101d22738e
SAGAR PURKUTI <sagar101purkuti@gmail.com>
2025-04-30 17:42:12 +0545

    change password to be continued

--- accounts/views.py
+++ accounts/views.py
@@ -240,7 +240,7 @@ def my_orders(request):
     }
     return render(request,'accounts/my_orders.html',context)
 
-
+@login_required(login_url='/login/')
 def edit_profile(request):
     userprofile = get_object_or_404(UserProfile, user=request.user)
     if request.method=='POST':
@@ -263,3 +263,29 @@ def edit_profile(request):
         'userprofile': userprofile,
     }
     return render(request, 'accounts/edit_profile.html', context)
+
+@login_required(login_url='/login/')
+def change_password(request):
+    if request.method=='POST':
+        current_password        =   request.POST['current_password']
+        new_password            =   request.POST['new_password']
+        confirm_new_password    =   request.POST['confirm_new_password']
+
+        user    = Account.objects.get(username__exact=request.user.username)
+
+        if new_password == confirm_new_password:
+            #django's inbuilt: check_password, set_password function 
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                #auth.logout(request)
+                messages.success(request,'Passowrd Update Sccessfully')
+                return redirect('change_password')
+            else:
+                messages.error(request, 'Please Enter Valid current password')
+                return redirect('change_password')
+        else:
+            messages.error(request, 'Password Does not Match!')
+            return redirect('change_password')
+    return render (request,'accounts/change_password.html')
